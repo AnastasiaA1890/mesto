73d77723ff4689e3a88d49aac171df26dfbf361f
Anastasia Andreeva <nastiy1890@yandex.ru>
2022-01-15 13:12:02 -0800

    feat: Added saving popup fields data

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
 const profileOpenPopupButton = document.querySelector('.profile__button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
-let profileFieldName = document.querySelector('.popup__field-name');
-let profileFieldDescription = document.querySelector('.popup__field-about');
+let popupFieldName = document.querySelector('.popup__field-name');
+let popupFieldDescription = document.querySelector('.popup__field-about');
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+const popupForm = document.querySelector('.popup__form');
 
 //Функция открытия окна Popup
 function openPopup () {
@@ -21,11 +24,22 @@ popupCloseButton.addEventListener('click', closePopup);
 
 //Функция очистки полей после закрытия окна Popup
 function cleanPopup() {
-    popupCloseButton.insertAdjacentText('afterbegin', profileFieldName.value);
-    popupCloseButton.insertAdjacentText('afterbegin', profileFieldDescription.value);
-    profileFieldName.value = '';
-    profileFieldDescription.value = '';
+    popupCloseButton.insertAdjacentText('afterbegin', popupFieldName.value);
+    popupCloseButton.insertAdjacentText('afterbegin', popupFieldDescription.value);
+    popupFieldName.value = '';
+    popupFieldDescription.value = '';
 }
 
 //Вызов функции очистки полей после закрытия окна Popup
 popupCloseButton.addEventListener('click', cleanPopup);
+
+//Функция сохранения данных полей формы Popup
+function submitFormHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = popupFieldName.value;
+    profileDescription.textContent = popupFieldDescription.value;
+    closePopup();
+}
+
+//Вызов функции сохранения данных полей формы Popup
+popupForm.addEventListener('submit', submitFormHandler);
