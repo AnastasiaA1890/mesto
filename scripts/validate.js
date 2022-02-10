04c3d305d1cca7cf9108bba190be2b03969cae42
Anastasia Andreeva <nastiy1890@yandex.ru>
2022-02-09 22:24:53 -0800

    feature: added new functionality

--- scripts/validate.js
+++ scripts/validate.js
@@ -72,6 +72,6 @@ enableValidation({
   submitButtonSelector: '.popup__button',
   inactiveButtonClass: 'popup__button_disabled',
   inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible_active'
+  errorClass: 'popup__error-visible_active'
 });
 
